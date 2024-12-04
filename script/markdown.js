ffec63eef8bd82290e26628af1a1f5a15f2ff0b1
Elvin-Mworia <elvinmworia@gmail.com>
2024-12-04 16:53:37 +0300

    compiled js version

--- script/markdown.js
+++ script/markdown.js
@@ -1,18 +1,27 @@
 "use strict";
 class HtmlHandler {
+    constructor() {
+        this.markdownChange = new Markdown;
+    }
     TextChangeHandler(id, output) {
         let markdown = document.getElementById(id);
         let markdownOutput = document.getElementById(output);
         if (markdown !== null) {
             markdown.onkeyup = (e) => {
-                if (markdown.value) {
-                    markdownOutput.innerHTML = markdown.value;
-                }
-                else {
-                    markdownOutput.innerHTML = "<p></p>";
-                }
+                this.RenderHtmlContent(markdown, markdownOutput);
+            };
+            //persisting the content on the page
+            window.onload = (e) => {
+                this.RenderHtmlContent(markdown, markdownOutput);
             };
         }
+    }
+    RenderHtmlContent(markdown, markdownOutput) {
+        if (markdown.value) {
+            markdownOutput.innerHTML = this.markdownChange.ToHtml(markdown.value);
+        }
+        else
+            markdownOutput.innerHTML = "<p></p>";
     }
 }
 var TagType;
@@ -63,6 +72,165 @@ class MarkdownDocument {
 class ParseElement {
     constructor() {
         this.CurrentLine = "";
+    }
+}
+class VisitorBase {
+    constructor(tagType, TagTypeToHtml) {
+        this.tagType = tagType;
+        this.TagTypeToHtml = TagTypeToHtml;
+    }
+    Visit(token, markdownDocument) {
+        markdownDocument.Add(this.TagTypeToHtml.OpeningTag(this.tagType), token.CurrentLine, this.TagTypeToHtml.ClosingTag(this.tagType));
+    }
+}
+class Header1Visitor extends VisitorBase {
+    constructor() {
+        super(TagType.Header1, new TagTypeToHtml());
+    }
+}
+class Header2Visitor extends VisitorBase {
+    constructor() {
+        super(TagType.Header2, new TagTypeToHtml());
+    }
+}
+class Header3Visitor extends VisitorBase {
+    constructor() {
+        super(TagType.Header3, new TagTypeToHtml());
+    }
+}
+class ParagraphVisitor extends VisitorBase {
+    constructor() {
+        super(TagType.Paragraph, new TagTypeToHtml());
+    }
+}
+class HorizontalRuleVisitor extends VisitorBase {
+    constructor() {
+        super(TagType.HorizontalRule, new TagTypeToHtml());
+    }
+}
+class Visitable {
+    Accept(visitor, token, markdownDocument) {
+        visitor.Visit(token, markdownDocument);
+    }
+}
+class Handler {
+    constructor() {
+        this.next = null;
+    }
+    SetNext(next) {
+        this.next = next;
+    }
+    HandleRequest(request) {
+        if (!this.CanHandle(request)) {
+            if (this.next !== null) {
+                this.next.HandleRequest(request);
+            }
+            return;
+        }
+    }
+}
+/**looks to see if the text starts with a tag. If it
+does, we set the boolean part of our tuple to true and use substr to get the
+remainder of our text. */
+class LineParser {
+    Parse(value, tag) {
+        let output = [false, ""];
+        output[1] = value;
+        if (value === "") {
+            return output;
+        }
+        let split = value.startsWith(`${tag}`);
+        if (split) {
+            output[0] = true;
+            output[1] = value.substr(tag.length);
+        }
+        return output;
+    }
+}
+class ParseChainHandler extends Handler {
+    constructor(document, tagType, visitor) {
+        super();
+        this.document = document;
+        this.tagType = tagType;
+        this.visitor = visitor;
+        this.visitable = new Visitable();
+    }
+    CanHandle(request) {
+        let split = new LineParser().Parse(request.CurrentLine, this.tagType);
+        if (split[0]) {
+            request.CurrentLine = split[1];
+            this.visitable.Accept(this.visitor, request, this.document);
+        }
+        return split[0];
+    }
+}
+//handler to handle paragraphs
+class ParagraphHandler extends Handler {
+    constructor(document) {
+        super();
+        this.document = document;
+        this.visitable = new Visitable();
+        this.visitor = new ParagraphVisitor();
+    }
+    CanHandle(request) {
+        this.visitable.Accept(this.visitor, request, this.document);
+        return true;
+    }
+}
+//handler for h1 with associated "#" tag
+class Header1ChainHandler extends ParseChainHandler {
+    constructor(document) {
+        super(document, "# ", new Header1Visitor());
+    }
+}
+//handler for h2 with associated "##" tag
+class Header2ChainHandler extends ParseChainHandler {
+    constructor(document) {
+        super(document, "## ", new Header2Visitor());
+    }
+}
+//handler for h3 with associated "###" tag                  
+class Header3ChainHandler extends ParseChainHandler {
+    constructor(document) {
+        super(document, "### ", new Header3Visitor());
+    }
+}
+//handler for hr with associated "---" tag
+class HorizontalRuleHandler extends ParseChainHandler {
+    constructor(document) {
+        super(document, "---", new HorizontalRuleVisitor());
+    }
+}
+class ChainOfResponsibilityFactory {
+    Build(document) {
+        let header1 = new Header1ChainHandler(document);
+        let header2 = new Header2ChainHandler(document);
+        let header3 = new Header3ChainHandler(document);
+        let horizontalRule = new HorizontalRuleHandler(document);
+        let paragraph = new ParagraphHandler(document);
+        header1.SetNext(header2);
+        header2.SetNext(header3);
+        header3.SetNext(horizontalRule);
+        horizontalRule.SetNext(paragraph);
+        return header1;
+    }
+}
+/**takes the text that the user is typing in and splits it into individual lines,
+ and creates our ParseElement,chain-of-responsibility handlers, and MarkdownDocument instance.
+ Each line is then forwarded to Header1ChainHandler to start the processing of the line.
+Finally, we get the text from the document and return it so that we can
+display it in the label */
+class Markdown {
+    ToHtml(text) {
+        let document = new MarkdownDocument();
+        let header1 = new ChainOfResponsibilityFactory().Build(document);
+        let lines = text.split(`\n`);
+        for (let index = 0; index < lines.length; index++) {
+            let parseElement = new ParseElement();
+            parseElement.CurrentLine = lines[index];
+            header1.HandleRequest(parseElement);
+        }
+        return document.Get();
     }
 }
 //# sourceMappingURL=markdown.js.map
